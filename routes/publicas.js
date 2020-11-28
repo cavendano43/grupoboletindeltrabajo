ce6d41faa2f4c38f9991c1ef054cb5b0c7faef5b
cavendano43 <cavendano@boletindeltrabajo.cl>
2020-11-28 20:29:27 -0300

    two commit

--- routes/publicas.js
+++ routes/publicas.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const router = express.Router()
-
+var multer  = require('multer');
+/////////////////////////// controllers ///////////////////////
+const FiniquitoController = require("../controllers/FiniquitoController");
+const DocumentosController = require("../controllers/DocumentosController");
 
 router.get("/",(peticion,respuesta)=>{
     respuesta.send('https://grupoboletindeltrabajo-b871d.web.app/');
 })
 
+/////////////////////// portal de soluciones ///////////////////
+router.post("/calculo-finiquito",FiniquitoController.calculo);
+router.post("/carta-finiquito",FiniquitoController.cartaAviso);
+router.post("/generar-finiquito",FiniquitoController.finiquito);
 
 module.exports = router
